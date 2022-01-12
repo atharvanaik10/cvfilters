79a2524740f13813ff8133ab61aa3dfde3a75c4d
Atharva Naik <atharvanaik10@gmail.com>
2022-01-12 18:40:40 +0530

    Try glasses

--- glasses.py
+++ glasses.py
@@ -1,9 +1,10 @@
 import cv2
 import nanocamera as nano
+import numpy as np
 
 def glasses():
 
-    glass = cv2.imread('glasses_overlay.png')
+    glass = cv2.imread('glasses_overlay.png', cv2.IMREAD_UNCHANGED)
     face_cascade = cv2.CascadeClassifier(
         "/usr/share/opencv4/haarcascades/haarcascade_frontalface_default.xml"
     )
@@ -19,10 +20,14 @@ def glasses():
         try:
             # read the camera image
             frame = camera.read()
+
             #convert to grayscale
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             #detect faces
             faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+            # add dummy alpha channel
+            frame = np.dstack([frame, np.zeros((frame.shape[0], frame.shape[1]))*255])
 
             for (x, y, w, h) in faces:
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
@@ -31,11 +36,13 @@ def glasses():
                 eyes = eye_cascade.detectMultiScale(roi_gray)
                 for (ex, ey, ew, eh) in eyes:
                     #try to put glasses instead of rectangle
-                    glass2 = cv2.resize(glass, dsize=(ew, eh))
-                    frame[ex + ew, ey + eh] = glass2
-                    # cv2.rectangle(
-                    #     roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2
-                    # )
+                    glass2 = cv2.resize(glass, dsize=(ew*2, eh))
+                    # frame[ey:ey + eh, ex:ex + (ew*2)] = glass2
+                    frame[0:eh, 0:ew*2] = glass2
+                    cv2.rectangle(
+                        roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2
+                    ) 
+                    break
             # display the frame
             cv2.imshow("Filter", frame)
             if cv2.waitKey(25) & 0xFF == ord('q'):
